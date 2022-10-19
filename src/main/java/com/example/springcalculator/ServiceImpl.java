fce55aa72d1d33b647518d0d336273a093e88833
Alexander Baklushin <din.myrk@gmail.com>
2022-10-20 00:48:34 +0300

    Spring 1.12 Написаны тесты

--- src/main/java/com/example/springcalculator/ServiceImpl.java
+++ src/main/java/com/example/springcalculator/ServiceImpl.java
@@ -1,7 +1,8 @@
 package com.example.springcalculator;
 
 @org.springframework.stereotype.Service
-public class ServiceImpl implements Service{
+public class ServiceImpl implements Service {
+
     @Override
     public int addition(int name1, int name2) {
         return name1 + name2;
@@ -14,19 +15,19 @@ public class ServiceImpl implements Service{
 
     @Override
     public int multiplication(int name1, int name2) {
-        return name1*name2;
+        return name1 * name2;
     }
 
     @Override
     public int division(int name1, int name2) throws IllegalArgumentException {
-        if (name2 == 0){
+        if (name2 == 0) {
             throw new IllegalArgumentException();
         }
-        return name1/name2;
+        return name1 / name2;
     }
 
     @Override
-    public String getMessage(){
+    public String getMessage() {
         return "You forgot the argument";
     }
 }
---
+++ src/test/java/com/example/springcalculator/ServiceTest.java
@@ -0,0 +1,60 @@
+package com.example.springcalculator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ServiceTest {
+
+    public int[] expectedForAdd;
+    public int[] expectedForSub;
+    public int[] expectedForMultiplication;
+    public int[] expectedForDivision;
+
+    @BeforeEach
+    public void setUp() {
+        expectedForAdd = new int[]{10, 5};
+        expectedForSub = new int[]{4,10};
+        expectedForMultiplication = new int[]{16,25};
+        expectedForDivision = new int[]{7,14};
+    }
+
+    @Test
+    public void additionTest() {
+        Service service = new ServiceImpl();
+        int[] actual = new int[]{service.addition(5, 5),service.addition(2,3)};
+        assertArrayEquals(expectedForAdd, actual);
+    }
+
+    @Test
+    public void subtractionTest() {
+        Service service = new ServiceImpl();
+        int[] actual = new int[]{service.subtraction(10, 6),service.subtraction(11,1)};
+        assertArrayEquals(expectedForSub, actual);
+    }
+
+    @Test
+    public void multiplicationTest() {
+        Service service = new ServiceImpl();
+        int[] actual = new int[]{service.multiplication(8, 2),service.multiplication(5,5)};
+        assertArrayEquals(expectedForMultiplication, actual);
+    }
+
+    @Test
+    public void divisionTest() {
+        Service service = new ServiceImpl();
+        int[] actual = new int[]{service.division(56,8),service.division(28,2)};
+        assertArrayEquals(expectedForDivision, actual);
+    }
+    @Test
+    public void divisionExceptionTest(){
+        Service service = new ServiceImpl();
+        IllegalArgumentException thrown = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            service.division(5,0);
+        });
+
+        Assertions.assertEquals("java.lang.IllegalArgumentException", thrown.toString());
+    }
+}
